f4abf28358ec21799705c40024b18f7183b86205
parallels <korey_kolberg@homedepot.com>
2017-09-18 14:33:25 -0400

    remove

--- fullstack/server/src/api/todo/model/todo-model.ts
+++
@@ -1,8 +0,0 @@
-import * as mongoose from "mongoose";
-
-let schema = new mongoose.Schema({
-    todoMessage: {type: String, required: true, trim: true},
-    createdAt: {type: Date, default: Date.now}
-});
-
-export default schema;
--- fullstack/tasks/server/index.js
+++
@@ -1,4 +0,0 @@
-import gulp from 'gulp';
-var gulpSequence = require('gulp-sequence')
-
-gulp.task('server.compile_server', gulpSequence('server.compile_tsc', 'server.compile_tsc_tests'));
