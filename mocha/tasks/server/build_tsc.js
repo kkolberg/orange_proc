0aedff2f1ca37e52f348ff767dfc5b73df5eaf85
parallels <korey_kolberg@homedepot.com>
2017-09-17 23:27:08 -0400

    debugging

--- mocha/tasks/server/build_tsc.js
+++ mocha/tasks/server/build_tsc.js
@@ -1,13 +1,32 @@
 import gulp from "gulp";
 import tsc from "gulp-typescript";
+var merge = require('merge-stream');
+const TS_CONFIG = "tsconfig.json";
+import sourcemaps from 'gulp-sourcemaps';
+const path = require('path');
 
-const TS_CONFIG = "./tsconfig.json";
+var clean = require('gulp-clean');
 
-gulp.task("server.compile_tsc", () => {
+gulp.task('clean-scripts', function () {
+  return gulp.src('dist', { read: false })
+    .pipe(clean());
+});
+
+gulp.task("server.compile_tsc", ['clean-scripts'], () => {
   let tsconfigSrc = tsc.createProject(TS_CONFIG);
-
-  return tsconfigSrc.src()
+  let server = tsconfigSrc.src()
+    .pipe(sourcemaps.init())
     .pipe(tsconfigSrc())
     .js
-    .pipe(gulp.dest("./dist"));
+    .pipe(sourcemaps.write({
+      sourceRoot: function (file) {
+        return path.relative(path.join(file.cwd, file.path), file.base);
+      }
+    }))
+    .pipe(gulp.dest("dist"));
+
+  let constants = gulp.src('server/constants/*')
+    .pipe(gulp.dest('dist/server/constants'));
+
+  return merge(server, constants);
 });
